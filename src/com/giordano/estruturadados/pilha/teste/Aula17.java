db87769c27b720fc89bf4a4c296f5a04abf7aa20
giordanonascimento <giordano.amaral@gnsystems.com.br>
2022-05-11 19:06:01 -0300

    Finalzando classe Pilha!

---
+++ src/com/giordano/estruturadados/pilha/teste/Aula17.java
@@ -0,0 +1,21 @@
+package com.giordano.estruturadados.pilha.teste;
+
+import com.giordano.estruturadados.pilha.Pilha;
+
+public class Aula17 {
+
+    public static void main(String[] args) {
+
+	Pilha<Integer> pilha = new Pilha<>();
+
+	pilha.empilha(1);
+	pilha.empilha(2);
+	pilha.empilha(3);
+
+	System.out.println(pilha);
+
+	System.out.println("Desempilhando elemento " + pilha.desempilha());
+	System.out.println(pilha);
+    }
+
+}
